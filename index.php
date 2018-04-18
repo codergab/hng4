3cf655f673fc6d6cb34307be3f3fc939f5cf17c6
Gabriel Adewumi <codergab@gmail.com>
2018-04-18 20:21:43 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>HNG4</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet"> 
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+</head>
+<body>
+    <div class="wrapper_split">
+        <div class="col-6 with-bg">
+            <div class="date"><?php echo date('h:i a'); ?></div>
+        </div>
+        <div class="col-6">
+            <div class="box-side">
+                <h1>HNG Internship 4</h1>
+                <p>Kickstart your coding career.</p>
+                <button class="button is-primary">Get Started</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
